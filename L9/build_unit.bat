3b7bb82094439c27603322d5cc8d1f5ae9eb4bd6
cottieda <cottieda@gmail.com>
2019-03-25 20:29:59 -0500

    Some Discriminant code initial pass

--- L9/build_unit.bat
+++ L9/build_unit.bat
@@ -3,4 +3,5 @@ cd %~dp0
 
 REM Pass the current directory to the R file so R can knit all R files in ./source
 Rscript.exe ../_script/build_unit.r %~dp0
+REM Rscript.exe "C:/Stat/New/Stat 505/r-mv-examples/_script" %~dp0
 pause
